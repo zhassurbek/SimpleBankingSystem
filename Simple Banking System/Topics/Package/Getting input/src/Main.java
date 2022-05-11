2f4558068237e67089dabfccbc221a604885843a
zhassurbek <zhassurbek.boltabayev.2000@gmail.com>
2022-05-11 10:58:46 +0600

    Package in Java DONE

---
+++ Simple Banking System/Topics/Package/Getting input/src/Main.java
@@ -0,0 +1,9 @@
+
+class InputReader {
+    public static String getString() {
+        // write your code here
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        String line = sc.nextLine();
+        return line;
+    }
+}
