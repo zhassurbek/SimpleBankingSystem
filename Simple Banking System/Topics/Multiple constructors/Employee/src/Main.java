d8758a464a67abbdeb145b738e16354a03fc32ce
zhassurbek <zhassurbek.boltabayev.2000@gmail.com>
2022-05-12 13:46:47 +0600

    Multiple constructors DONE

---
+++ Simple Banking System/Topics/Multiple constructors/Employee/src/Main.java
@@ -0,0 +1,25 @@
+
+class Employee {
+    String name;
+    int salary;
+    String address;
+
+
+    public Employee() {
+        name = "unknown";
+        salary = 0;
+        address = "unknown";
+    }
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+        this.address = "unknown";
+    }
+
+    public Employee(String name, int salary, String address) {
+        this.name = name;
+        this.salary = salary;
+        this.address = address;
+    }
+}
